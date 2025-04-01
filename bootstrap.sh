93b34ac4240c088be7c9c416b47ef9f70a57b310
motOko <dourval.clement@gmail.com>
2025-04-01 12:45:32 +0200

    adding test to process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# pip install ./src/python/
-# pytest src/python/tests --junitxml=python-report.xml
-
 rm -rf ./dist
 hatch build
+
+pip install .
+pytest ./tests --junitxml=python-report.xml
