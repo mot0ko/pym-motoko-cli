74c61ded9dcf35d495fbd062fa906faef6416946
motOko <dourval.clement@gmail.com>
2025-04-15 16:45:36 +0200

    credentials were removed, retrying dedicated step

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # python -m pip install --upgrade pip
 # pip install twine hatch numpy poetry pytest setuptools wheel
-pip install twine
+# pip install twine
 
 rm -rf ./dist
 hatch build
@@ -12,5 +12,5 @@ pytest ./tests --junitxml=python-report.xml
 
 # twine upload --repository-url "https://pypi.org/project/motoko-cli/" ./dist/*
 # twine upload ./dist/*
-echo "twine upload -u '$TWINE_USERNAME' -p '$TWINE_PASSWORD' ./dist/*"
-twine upload -u "$TWINE_USERNAME" -p "$TWINE_PASSWORD" ./dist/*
+# echo "twine upload -u '$TWINE_USERNAME' -p '$TWINE_PASSWORD' ./dist/*"
+# twine upload -u "$TWINE_USERNAME" -p "$TWINE_PASSWORD" ./dist/*
