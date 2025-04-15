62064e7a13cf3339666471fbb12e0b52950b78e7
motoko <dourval.clement@gmail.com>
2025-04-15 23:43:24 +0200

    adding git short hash only when running in CI job

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,17 @@
 # pip install twine hatch numpy poetry pytest setuptools wheel
 # pip install twine
 
+git config --global --add safe.directory /app
+
+version_file="src/motoko_cli/__about__.py"
+tmp_version_file="src/motoko_cli/__about__-tmp.py"
+base_version="0.0.1rc0.dev0"
+git_hash=$(git rev-parse --short HEAD)
+
+if [ "$CI" != "" ]; then
+  python -c "open('$tmp_version_file', 'w', encoding='utf-8').write(open('$version_file', 'r', encoding='utf-8').read().replace('$base_version', '0.0.1rc0.dev0+$git_hash')); import shutil; shutil.move('$tmp_version_file', '$version_file')"
+fi
+
 rm -rf ./dist
 hatch build
 
