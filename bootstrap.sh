ccb470b0d77d47fd95d9629b36ee57a71e33721e
motoko <dourval.clement@gmail.com>
2025-04-17 17:04:33 +0200

    removing twine upload from build script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# python -m pip install --upgrade pip
-# pip install twine hatch numpy poetry pytest setuptools wheel
-# pip install twine
-
 if [ "$CI" != "" ]; then
   git config --global --add safe.directory /app
   git config --global --add safe.directory /github/workspace
@@ -23,8 +19,3 @@ hatch build
 
 pip install .
 pytest ./tests --junitxml=python-report.xml
-
-# twine upload --repository-url "https://pypi.org/project/motoko-cli/" ./dist/*
-# twine upload ./dist/*
-# echo "twine upload -u '$TWINE_USERNAME' -p '$TWINE_PASSWORD' ./dist/*"
-# twine upload -u "$TWINE_USERNAME" -p "$TWINE_PASSWORD" ./dist/*
