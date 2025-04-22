5b5790f52632ad45a71fd39161b034f60954e3f2
motOko <dourval.clement@gmail.com>
2025-04-22 14:50:37 +0200

    new main branch version number generation

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,10 @@ base_version="0.0.1rc0.dev0"
 
 if [ "$CI" != "" ]; then
   git_hash=$(git rev-parse --short HEAD)
-  python -c "open('$tmp_version_file', 'w', encoding='utf-8').write(open('$version_file', 'r', encoding='utf-8').read().replace('$base_version', '0.0.1rc0.dev0+$git_hash')); import shutil; shutil.move('$tmp_version_file', '$version_file')"
+  latest_tag=$(git describe --tags --abbrev=0)
+  new_version=$(python -c "print('$latest_tag'.split('.dev')[0] + '.dev' + str(int('$latest_tag'.split('.dev')[1])+1))")
+  echo "New version will be $new_version"
+  python -c "open('$tmp_version_file', 'w', encoding='utf-8').write(open('$version_file', 'r', encoding='utf-8').read().replace('$base_version', '$new_version')); import shutil; shutil.move('$tmp_version_file', '$version_file')"
 fi
 
 rm -rf ./dist
