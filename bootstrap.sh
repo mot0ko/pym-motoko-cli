5557e4668d76b43925cfadcb15de60bbd582df6f
motOko <dourval.clement@gmail.com>
2025-04-22 14:09:26 +0200

    moving git action to CI block

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,9 @@ fi
 version_file="src/motoko_cli/__about__.py"
 tmp_version_file="src/motoko_cli/__about__-tmp.py"
 base_version="0.0.1rc0.dev0"
-git_hash=$(git rev-parse --short HEAD)
 
 if [ "$CI" != "" ]; then
+  git_hash=$(git rev-parse --short HEAD)
   python -c "open('$tmp_version_file', 'w', encoding='utf-8').write(open('$version_file', 'r', encoding='utf-8').read().replace('$base_version', '0.0.1rc0.dev0+$git_hash')); import shutil; shutil.move('$tmp_version_file', '$version_file')"
 fi
 
